3da4b83bf44e6a3f9925b534a88a8049e932b691
Захар <carroll.lewi@gmail.com>
2023-10-30 17:10:32 +0200

    add all models

--- backend/store/product/models.py
+++ backend/store/product/models.py
@@ -1,3 +1,88 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True, )
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.PROTECT)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return f'{self.name}|{self.category.name}'
+
+
+class ProductSpecs(models.Model):
+    name = models.CharField(max_length=256)
+    value = models.CharField(max_length=256)
+    product_id = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+
+
+class ProductReview(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    rating = models.PositiveIntegerField(default=1, validators=[MaxValueValidator(5)])
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class BasketQuerySet(models.QuerySet):
+    def total_sum(self):
+        return sum(basket.sum() for basket in self)
+
+    def total_quantity(self):
+        return sum(basket.quantity for basket in self)
+
+    def stripe_products(self):
+        line_items = []
+        for basket in self:
+            item = {
+                'price': basket.product.stripe_product_price_id,
+                'quantity': basket.quantity,
+            }
+            line_items.append(item)
+        return line_items
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = BasketQuerySet.as_manager()
+
+    def __str__(self):
+        return f'Basket for {self.user.username} | Product: {self.product.name}'
+
+    def sum(self):
+        return self.product.price * self.quantity
+
+    def de_json(self):
+        basket_item = {
+            'product_name': self.product.name,
+            'quantity': self.quantity,
+            'price': float(self.product.price),
+            'sum': float(self.sum()),
+        }
+        return basket_item
+
--- backend/store/user/models.py
+++ backend/store/user/models.py
@@ -1,3 +1,40 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.core.mail import send_mail
 from django.db import models
+from django.urls import reverse
+from django.utils.timezone import now
 
-# Create your models here.
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', null=True, blank=True)
+    is_verified_email = models.BooleanField(default=False)
+
+
+class EmailVerification(models.Model):
+    code = models.UUIDField(unique=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    expiration = models.DateTimeField()
+
+    def __str__(self):
+        return f'EmailVerification object for {self.user.email}'
+
+    def send_verification_email(self):
+        link = reverse('users:email_verification', kwargs={'email': self.user.email, 'code': self.code})
+        verification_link = f'{settings.DOMAIN_NAME}{link}'
+        subject = f'Account confirmation for {self.user.username}'
+        massage = 'Follow the link {} to confirm the account for user {}'.format(
+            verification_link,
+            self.user.email
+        )
+        send_mail(
+            subject=subject,
+            message=massage,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[self.user.email],
+            fail_silently=False,
+        )
+
+    def is_expired(self):
+        return True if now() >= self.expiration else False
