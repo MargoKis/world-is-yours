c55f954c3c6360e4d15197832b8058606b830865
Захар <carroll.lewi@gmail.com>
2023-11-05 15:40:37 +0200

    add Admin panel

--- backend/store/product/admin.py
+++ backend/store/product/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from product.models import Product, ProductCategory, ProductSpecs, ProductReview
 
-# Register your models here.
+
+@admin.register(Product)
+class TaskModelAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(ProductCategory)
+class TaskModelAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(ProductSpecs)
+class TaskModelAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(ProductReview)
+class TaskModelAdmin(admin.ModelAdmin):
+    ...
+
