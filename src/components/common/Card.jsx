e29b8b00df3d3ae2d7363d8952a5a3cc273babb6
Marharyta <margaritakislajalemon@gmail.com>
2023-11-30 20:05:52 +0200

    work with api

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -1,30 +1,79 @@
+// import React from "react";
+// import TentImg from "../../assets/temporary-img/tent.png";
+// import HeartIcon from "../../assets/icons/icon-heart.svg";
+// import Button from "./Button";
+// import Cart from '../../assets/icons/dark/icon-cart-dark.svg'
+
+// const Card = ({ data }) => {
+//   return (
+//     <div className=" m-3 border-2 border-gray-light rounded-lg relative"> 
+//       <img
+//         src={TentImg}
+//         alt=""
+//         className="w-21 max-w-200 rounded-lg"
+//       />
+//       <div className="absolute top-3 right-3 m-2">
+//         <img src={HeartIcon} alt="heart icon" width="26" />
+//       </div>
+//       <div className="flex flex-row justify-between items-center">
+//       <div className="p-5 ">
+//         <p className="text-custom-black">{data.name}</p>
+//         <p className="text-custom-black">{data.price}</p>
+//         <p className="text-custom-black">{data.colors.join(", ")}</p>
+//       </div>
+//       <Button classNameBtn='flex border rounded-md items-center py-3 px-3 mr-2'><img src={Cart} alt="cart"/></Button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Card;
+
+
+
 import React from "react";
-import TentImg from "../../assets/temporary-img/tent.png";
 import HeartIcon from "../../assets/icons/icon-heart.svg";
 import Button from "./Button";
-import Cart from '../../assets/icons/dark/icon-cart-dark.svg'
+import Cart from '../../assets/icons/dark/icon-cart-dark.svg';
 
 const Card = ({ data }) => {
   return (
-    <div className=" m-3 border-2 border-gray-light rounded-lg relative"> 
-      <img
-        src={TentImg}
-        alt=""
-        className="w-21 max-w-200 rounded-lg"
-      />
-      <div className="absolute top-3 right-3 m-2">
-        <img src={HeartIcon} alt="heart icon" width="26" />
+    <div className="m-3 border-2 border-gray-light rounded-lg relative flex flex-col">
+      <div className="relative">
+        <img
+          src={data.image} 
+          alt=""
+          className="w-72 max-w-200 rounded-lg"
+        />
+        <div className="absolute top-3 right-3 m-2">
+          <img src={HeartIcon} alt="heart icon" width="26" />
+        </div>
       </div>
-      <div className="flex flex-row justify-between items-center">
-      <div className="p-5 ">
-        <p className="text-custom-black">{data.name}</p>
-        <p className="text-custom-black">{data.price}</p>
-        <p className="text-custom-black">{data.colors.join(", ")}</p>
+      <div className="flex flex-col p-5 justify-between flex-grow">
+        <div>
+          <p className="text-custom-black font-semibold">{data.name}</p>
+          <p className="text-grayLight">{data.mass}</p>
+        </div>
+        <div className="flex justify-between items-center">
+          <p className="text-custom-black">{data.gender}</p>
+          <div className="flex items-center">
+            {Array.isArray(data.colors) &&
+              data.colors.map((color, index) => (
+                <div
+                  key={index}
+                  className="w-4 h-4 rounded-full mr-1"
+                  style={{ backgroundColor: color, border: "1px solid #000" }}
+                ></div>
+              ))}
+          </div>
+        </div>
       </div>
-      <Button classNameBtn='flex border rounded-md items-center py-3 px-3 mr-2'><img src={Cart} alt="cart"/></Button>
-      </div>
+      <Button classNameBtn='flex border rounded-md items-center py-3 px-3 mr-2'>
+        <img src={Cart} alt="cart"/>
+      </Button>
     </div>
   );
 };
 
 export default Card;
+
--- src/components/main-page/FavouritesCardList.jsx
+++ src/components/main-page/FavouritesCardList.jsx
@@ -1,29 +1,73 @@
+// import React, { useEffect, useState } from "react";
+// import Card from "../common/Card";
+// import cardData from "../../data";
+// import axios from 'axios'
+
+// const FavoritesCardList = () => {
+//   const favoriteCards = cardData.slice(0, 8);
+
+//   const [products, setProducts] = useState();
+
+//   useEffect(() => {
+//     const API = 'http://localhost:8000/api/products/'
+//     axios
+//       .get(API)
+//       .then((response) => {
+//         const favourites = response.data;
+//         setProducts(favourites)
+//       }, [setProducts])
+//   })
+
+//   return (
+//     <div className="text-grayLight">
+//       <h1 className="flex items-center justify-center mt-20 mb-10 font-raleway text-custom-black text-30px">Наші фаворити</h1>
+//       <div className="flex flex-wrap justify-around mb-20 mx-6">
+//         {favoriteCards.map((item, index) => (
+//           <Card data={item} key={index} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FavoritesCardList;
+
+
+
+
+
+
 import React, { useEffect, useState } from "react";
 import Card from "../common/Card";
-import cardData from "../../data";
-import axios from 'axios'
+import axios from 'axios';
 
 const FavoritesCardList = () => {
-  const favoriteCards = cardData.slice(0, 8);
-
-  const [products, setProducts] = useState();
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
+    // const API = 'https://swapi.dev/api/people/';
+
     const API = 'http://localhost:8000/api/products/'
-    axios
-      .get(API)
+
+    axios.get(API)
       .then((response) => {
-        const favourites = response.data;
-        setProducts(favourites)
-      }, [setProducts])
-  })
+        const favourites = response.data; // Получаем только массив results
+        setProducts(favourites); // Устанавливаем массив results в products
+      })
+      .catch((error) => {
+        console.error('Помилка при отриманні даних:', error);
+      });
+  }, []); 
 
   return (
     <div className="text-grayLight">
       <h1 className="flex items-center justify-center mt-20 mb-10 font-raleway text-custom-black text-30px">Наші фаворити</h1>
       <div className="flex flex-wrap justify-around mb-20 mx-6">
-        {favoriteCards.map((item, index) => (
-          <Card data={item} key={index} />
+        {products.map((item, index) => (
+          <Card
+            key={index} // Используйте уникальный ключ, например, index
+            data={item}
+          />
         ))}
       </div>
     </div>
