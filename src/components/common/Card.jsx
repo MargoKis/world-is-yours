03546f82e9e8f80b57688657f468883b26dd12e1
Marharyta <margaritakislajalemon@gmail.com>
2023-12-17 21:50:40 +0200

    change Card

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -1,36 +1,3 @@
-// import React from "react";
-// import TentImg from "../../assets/temporary-img/tent.png";
-// import HeartIcon from "../../assets/icons/icon-heart.svg";
-// import Button from "./Button";
-// import Cart from '../../assets/icons/dark/icon-cart-dark.svg'
-
-// const Card = ({ data }) => {
-//   return (
-//     <div className=" m-3 border-2 border-gray-light rounded-lg relative"> 
-//       <img
-//         src={TentImg}
-//         alt=""
-//         className="w-21 max-w-200 rounded-lg"
-//       />
-//       <div className="absolute top-3 right-3 m-2">
-//         <img src={HeartIcon} alt="heart icon" width="26" />
-//       </div>
-//       <div className="flex flex-row justify-between items-center">
-//       <div className="p-5 ">
-//         <p className="text-custom-black">{data.name}</p>
-//         <p className="text-custom-black">{data.price}</p>
-//         <p className="text-custom-black">{data.colors.join(", ")}</p>
-//       </div>
-//       <Button classNameBtn='flex border rounded-md items-center py-3 px-3 mr-2'><img src={Cart} alt="cart"/></Button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Card;
-
-
-
 import React from "react";
 import HeartIcon from "../../assets/icons/icon-heart.svg";
 import Button from "./Button";
@@ -40,7 +7,7 @@ import Image from '../../assets/temporary-img/tent.png'
 const Card = ({ data }) => {
   return (
     <>
-    <div className=" m-3 border-2 border-gray-light rounded-lg relative w-80">
+    <div className=" m-3 border-2 border-gray-light rounded-lg relative w-80 items-center">
         <img
           src={Image} 
           alt=""
@@ -50,25 +17,14 @@ const Card = ({ data }) => {
           <img src={HeartIcon} alt="heart icon" width="26" className='cursor-pointer'/>
         </div>
     
-        <div className="flex flex-col">
-        <div className="p-5 ">
+        <div className="flex flex-row justify-between items-center">
+        <div className="p-5 flex-col">
           <p className="text-custom-black font-semibold">{data.name}</p>
           <p className="text-custom-black ">{data.price}</p>
-          <p className="text-custom-black ">{data.description}</p>
-          <div className="flex items-center">
-            {Array.isArray(data.colors) &&
-              data.colors.map((color, index) => (
-                <div
-                  key={index}
-                  className="w-4 h-4 rounded-full mr-1"
-                  style={{ backgroundColor: color, border: "1px solid #000" }}
-                ></div>
-              ))}
-          </div>
         </div>
-        <Button classNameBtn='flex justify-center border rounded-md items-center py-3 px-3'>
-        <img src={Cart} alt="cart"/>
-      </Button>
+        <Button classNameBtn='flex border rounded-md py-3 px-3 mr-4'>
+           <img src={Cart} alt="cart"/>
+        </Button>
       </div>
    
       </div>
