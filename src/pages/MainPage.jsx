9ab64a0f6b337898106facb43fb7a54c9337d672
Marharyta <margaritakislajalemon@gmail.com>
2024-01-02 23:18:41 +0200

    fix chat popup

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import ArrowIcon from "../assets/icons/icon-arrow.svg";
 import ChatIcon from "../assets/icons/button_chat.svg";
 
@@ -8,8 +9,14 @@ import SeasonSelect from "../components/main-page/SeasonSelect";
 import FavouritesCardList from "../components/main-page/FavouritesCardList";
 import NewArrivalsCardList from "../components/main-page/NewArrivalsCardList";
 import ScrollingText from "../components/scrolling-effect/ScrollingText";
+import ChatPopup from "../components/main-page/ChatPopup";
 
 const MainPage = () => {
+  const [isChatPopupOpen, setIsChatPopupOpen] = useState(false);
+
+  const toggleChatPopup = () => {
+    setIsChatPopupOpen(!isChatPopupOpen);
+  };
   return (
     <>
       <div
@@ -32,7 +39,16 @@ const MainPage = () => {
 
         {/* chat icon */}
         <div className={`flex flex-row-reverse h-1/5 items-center px-10 flex`}>
-          <img className="scale-75" src={ChatIcon} alt="Arrow Down" />
+        <img
+            className="scale-75 cursor-pointer"
+            src={ChatIcon}
+            alt="Arrow Down"
+            onClick={toggleChatPopup}
+          />
+
+          {isChatPopupOpen && (
+            <ChatPopup onClose={() => setIsChatPopupOpen(false)} />
+          )}
         </div>
       </div>
 
