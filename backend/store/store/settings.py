645058823e639625446bf02607f15783a7ee7625
Захар <carroll.lewi@gmail.com>
2024-03-04 16:38:44 +0200

    minor update

--- backend/store/store/settings.py
+++ backend/store/store/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'django-backend']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(" ")
 
 DOMAIN_NAME = os.getenv('DOMAIN_NAME')
 
