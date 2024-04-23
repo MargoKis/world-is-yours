8d5c59c3bab1ed6c29e5c398c9e3ea8f81c2ff3b
kamilka13me <kamilka13me@gmail.com>
2024-04-23 13:50:09 +0300

    upd: basket

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,57 +1,99 @@
-import React from 'react';
-import order from '../../assets/temporary-img/tent.png';
-import trash from '../../assets/icons/icon-trash.svg';
-import ArrowDown from '../../assets/icons/arrow-up.svg';
+import React, { useEffect } from "react";
+import order from "../../assets/temporary-img/tent.png";
+import trash from "../../assets/icons/icon-trash.svg";
+import ArrowDown from "../../assets/icons/arrow-up.svg";
 
 const CartItem = ({ product, handleQuantityChange, handleRemoveItem }) => {
   const calculateProductTotal = () => {
-    if (typeof product.discount === 'number') {
-      const discountedPrice = product.price * (1 - product.discount / 100);
-      return discountedPrice * product.quantity;
-    } else {
-      return product.price * product.quantity;
-    }
+    // if (typeof product.discount === "number") {
+    //   const discountedPrice = product.price * (1 - product.discount / 100);
+    //   return discountedPrice * product.quantity;
+    // } else {
+    //   return product.price * product.quantity;
+    // }
+    return null;
   };
+
+  // price
+  function calculateDiscountPercentage(oldPrice, price) {
+    if (oldPrice <= 0) {
+      return "Немає";
+    }
+    if (oldPrice <= price) {
+      return "Немає";
+    }
+    const discount = ((oldPrice - price) / oldPrice) * 100;
+
+    if (discount < 0) {
+      return "Немає";
+    }
+
+    return Math.round(discount) + "%";
+  }
+
+  useEffect(() => console.log(product), []);
 
   return (
     <>
-      <div className='flex flex-row justify-between items-end border-b border-gray p-4'>
-        <div className='flex flex-row items-center'>
-          <img src={order} alt={product.name} className='w-36 h-36 rounded-lg mr-4' />
-          <div className='flex flex-col gap-2'>
-            <p className='font-medium text-xl'>{product.name}</p>
-            <div className='text-gray gap-2'>
-              <p className='mt-3 text-grey font-normal text-xl'>Розмір: {product.size}</p>
-              <p className='mt-3 text-grey font-normal text-xl'>Колір: {product.color}</p>
-              <p className='mt-3 flex flex-row items-center text-gray font-normal text-xl'>
+      <div className="flex flex-row justify-between items-end border-b border-gray p-4">
+        <div className="flex flex-row items-center">
+          <img
+            src={product.image_1}
+            alt={product.name}
+            className="w-36 h-36 rounded-lg mr-4"
+          />
+          <div className="flex flex-col gap-2">
+            <p className="font-medium text-xl">{product.name}</p>
+            <div className="text-gray gap-2">
+              <p className="mt-3 text-grey font-normal text-xl">Розмір: {product.size}</p>
+              <p className="mt-3 text-grey font-normal text-xl">Колір: {product.color}</p>
+              <p className="mt-3 flex flex-row items-center text-gray font-normal text-xl">
                 Кількість:
                 <img
-                  className={`w-3 mr-2 duration-300 rotate-180 ml-4 mr-4 cursor-pointer focus:scale-150 ${product.quantity === 1 ? 'invert hover:cursor-default' : 'invert-0'}`}
+                  className={`w-3 mr-2 duration-300 rotate-180 ml-4 mr-4 cursor-pointer focus:scale-150 ${product.quantity === 1 ? "invert hover:cursor-default" : "invert-0"}`}
                   src={ArrowDown}
-                  alt='arrow down(remove one item)'
-                  onClick={() => handleQuantityChange(product.id, 'decrement')}
-                  tabIndex='0'
+                  alt="arrow down(remove one item)"
+                  onClick={() => handleQuantityChange(product.basketId, "decrement")}
+                  tabIndex="0"
                 />
-                <span className='text-black'>{product.quantity}</span>
-                <img className='w-3 mr-2 duration-300 rotate-0 ml-4 cursor-pointer focus:scale-150' src={ArrowDown} alt='arrow up(add one new item)' onClick={() => handleQuantityChange(product.id, 'increment')} tabIndex='0' />{' '}
+                <span className="text-black">{product.quantity}</span>
+                <img
+                  className="w-3 mr-2 duration-300 rotate-0 ml-4 cursor-pointer focus:scale-150"
+                  src={ArrowDown}
+                  alt="arrow up(add one new item)"
+                  onClick={() => handleQuantityChange(product.basketId, "increment")}
+                  tabIndex="0"
+                />{" "}
               </p>
             </div>
           </div>
         </div>
-        <div className='flex flex-col justify-between'>
-          <p className=''></p>
-          <div className=''>
-            <p className='text-gray mt-3 font-normal text-xl'>
-              Ціна: <span className='text-black'>{product.price} грн</span>
+        <div className="flex flex-col justify-between">
+          <p className=""></p>
+          <div className="">
+            <p className="text-gray mt-3 font-normal text-xl">
+              Ціна: <span className="text-black">{product.price} грн</span>
             </p>
-            <p className='text-gray mt-3 font-normal text-xl'>
-              Знижка: <span className='text-black'>{product.discount === 'Немає' ? product.discount : product.discount + '%'}</span>
+            <p className="text-gray mt-3 font-normal text-xl">
+              Знижка:{" "}
+              <span className="text-black">
+                {/* {product.discount === "Немає" ? product.discount : product.discount + "%"} */}
+                {calculateDiscountPercentage(product.old_price, product.price)}
+              </span>
             </p>
-            <p className='text-gray mt-3 font-semibold text-grayDark'>Всього: {calculateProductTotal()} грн</p>
+            <p className="text-gray mt-3 font-semibold text-grayDark">
+              Всього: {product.price} грн
+            </p>
           </div>
         </div>
         <div>
-          <img src={trash} alt='delete icon' className='cursor-pointer w-10 h-10 mb-9 p-2 border border-transparent rounded-lg duration-100 hover:border-slate-400 focus:border-slate-400' onClick={() => handleRemoveItem(product.id)} tabIndex='0' />
+          <img
+            src={trash}
+            alt="delete icon"
+            className="cursor-pointer w-10 h-10 mb-9 p-2 border border-transparent rounded-lg duration-100 hover:border-slate-400 focus:border-slate-400"
+            onClick={() => handleRemoveItem(product.basketId)}
+            tabIndex="0"
+          />
         </div>
       </div>
     </>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,108 +1,114 @@
-import React, { useState, useEffect } from 'react';
-import CartItem from '../components/cart/CartItem';
-import Button from '../components/common/Button';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import CartItem from "../components/cart/CartItem";
+import Button from "../components/common/Button";
+import { Link } from "react-router-dom";
 
-import { motion as m } from 'framer-motion';
+import { motion as m } from "framer-motion";
+import { $api } from "../api/api";
 
 const Cart = () => {
-  const [cart, setCart] = useState([
-    {
-      id: 1,
-      name: 'Палатка',
-      price: 100,
-      size: 'M',
-      color: 'Red',
-      quantity: 5,
-      discount: 10,
-    },
-    {
-      id: 2,
-      name: 'Футболка',
-      price: 200,
-      size: 'M',
-      color: 'White',
-      quantity: 3,
-      discount: 5,
-    },
-    {
-      id: 3,
-      name: 'Термос',
-      price: 225,
-      size: 'none',
-      color: 'Grey',
-      quantity: 2,
-      discount: 'Немає',
-    },
-    {
-      id: 4,
-      name: 'Кавоварка',
-      price: 415,
-      size: 'none',
-      color: 'Yellow',
-      quantity: 1,
-      discount: 15,
-    },
-    // Добавьте больше товаров при необходимости
-  ]);
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [upd, setUpd] = useState(0);
+  const [basket, setBasket] = useState(0);
 
-  const calculateTotalAmount = (cartItems) => {
-    let total = 0;
-    cartItems.forEach((item) => {
-      if (typeof item.discount === 'number') {
-        const discountedPrice = item.price * (1 - item.discount / 100);
-        total += discountedPrice * item.quantity;
-      } else {
-        total += item.price * item.quantity;
-      }
-    });
-    return total;
+  const fetchBasket = async () => {
+    try {
+      const basketResponse = await $api.get(`/api/baskets/`);
+      const basketItems = basketResponse.data;
+      setBasket(basketItems);
+      console.log(basketResponse.data);
+
+      let newTotal = 0; // Змінна для зберігання загальної вартості корзини
+
+      const productRequests = basketItems.map((item) =>
+        $api.get(`/api/products/${item.product}/`).then((response) => {
+          const productData = {
+            ...response.data,
+            quantity: item.quantity,
+            basketId: item.id,
+          };
+          newTotal += productData.price * item.quantity; // Додавання вартості цього продукту в загальну суму
+          return productData;
+        })
+      );
+
+      const products = await Promise.all(productRequests);
+      setCart(products);
+      setTotal(newTotal); // Оновлення стану загальної суми
+    } catch (error) {
+      console.log(error);
+    }
   };
-
-  const [totalAmount, setTotalAmount] = useState(calculateTotalAmount(cart));
 
   useEffect(() => {
-    setTotalAmount(calculateTotalAmount(cart));
-  }, [cart]);
+    fetchBasket();
+  }, [upd]);
 
-  const handleQuantityChange = (id, operation) => {
-    setCart((prevCart) => {
-      const updatedCart = prevCart.map((product) => {
-        if (product.id === id) {
-          let newQuantity = product.quantity;
-          if (operation === 'increment' && newQuantity < 100) {
-            newQuantity++;
-          } else if (operation === 'decrement' && newQuantity > 1) {
-            newQuantity--;
-          }
-          return { ...product, quantity: newQuantity };
-        }
-        return product;
+  const updBasket = async (id) => {
+    const quantity = 2;
+    try {
+      const basketUpdReq = await $api.put(`/api/products/${id}/`, quantity);
+      basketUpdReq.then((response) => {
+        console.log(response.data);
       });
-      return updatedCart;
-    });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleRemoveAllItems = async () => {
+    try {
+      console.log(basket);
+      // Створюємо масив промісів для кожного запиту на видалення
+      const deleteRequests = basket.map((item) => {
+        console.log(item);
+        $api.delete(`/api/baskets/${item.id}/`);
+      });
+
+      // Чекаємо на завершення всіх запитів
+      await Promise.all(deleteRequests);
+
+      console.log("All items removed");
+      setUpd(Math.floor(Math.random() * 100) + 1);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  const handleRemoveItem = (id) => {
-    setCart((prevCart) => {
-      const updatedCart = prevCart.filter((product) => product.id !== id);
-      return updatedCart;
-    });
+  const handleRemoveItem = async (id) => {
+    try {
+      await $api.delete(`/api/baskets/${id}/`).then((response) => {
+        setUpd(Math.floor(Math.random() * 100) + 1);
+      });
+    } catch (error) {
+      console.log(error);
+      setUpd(Math.floor(Math.random() * 100) + 1);
+    }
   };
 
-  const handleRemoveAllItems = () => {
-    setCart([]);
+  const handleQuantityChange = (productId, action) => {
+    if (action === "increment") {
+      console.log("increment", productId);
+      updBasket(productId);
+    } else if (action === "decrement") {
+      console.log("decrement", productId);
+    }
   };
 
   if (cart.length === 0) {
     return (
-      <div className='w-full flex justify-center items-center flex-col mt-20 mb-60'>
-        <p className='font-raleway font-semibold text-4xl mb-20'>Кошик</p>
-        <p className='font-raleway text-18px font-semibold text-center text-gray mt-10'>
+      <div className="w-full flex justify-center items-center flex-col mt-20 mb-60">
+        <p className="font-raleway font-semibold text-4xl mb-20">Кошик</p>
+        <p className="font-raleway text-18px font-semibold text-center text-gray mt-10">
           Кошик порожній. <br /> Додайте товари, які вас цікавлять!
         </p>
-        <Link to='/'>
-          <Button classNameBtn='w-22 bg-gray-dark mt-6 p-4 border rounded-xl font-bold text-18px text-white' nameBtn='submitForm' valueBtn='submit' type='submit'>
+        <Link to="/">
+          <Button
+            classNameBtn="w-22 bg-gray-dark mt-6 p-4 border rounded-xl font-bold text-18px text-white"
+            nameBtn="submitForm"
+            valueBtn="submit"
+            type="submit"
+          >
             На головну сторінку
           </Button>
         </Link>
@@ -111,35 +117,59 @@ const Cart = () => {
   }
 
   return (
-    <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.6 }}>
-      <div className='w-8/12 mx-auto mt-16 mb-10'>
-        <div className='flex justify-between items-center mb-4'>
-          <p className='font-raleway font-semibold text-4xl mx-auto'>Кошик</p>
-          <p className='font-raleway font-normal text-lg cursor-pointer hover:underline focus:underline' onClick={handleRemoveAllItems} tabIndex='0'>
+    <m.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.6 }}
+    >
+      <div className="w-8/12 mx-auto mt-16 mb-10">
+        <div className="flex justify-between items-center mb-4">
+          <p className="font-raleway font-semibold text-4xl mx-auto">Кошик</p>{" "}
+          <span className="hidden">{upd}</span>
+          <p
+            className="font-raleway font-normal text-lg cursor-pointer hover:underline focus:underline"
+            onClick={handleRemoveAllItems}
+            tabIndex="0"
+          >
             Видалити все
           </p>
         </div>
 
-        <div className='cart-container max-h-52 overflow-y-auto'>
+        <div className="cart-container max-h-52 overflow-y-auto">
           {cart.map((product) => (
-            <CartItem key={product.id} product={product} handleQuantityChange={handleQuantityChange} handleRemoveItem={handleRemoveItem} />
+            <CartItem
+              key={product.id}
+              product={product}
+              handleQuantityChange={handleQuantityChange}
+              handleRemoveItem={handleRemoveItem}
+            />
           ))}
         </div>
       </div>
-      <div className='w-10/12 mx-auto'>
-        <hr className='text-gray' />
+      <div className="w-10/12 mx-auto">
+        <hr className="text-gray" />
 
-        <div className='flex flex-row text-center items-center my-10'>
-          <Button classNameBtn='w-9/12 bg-transparent border-blue border-dashed text-blue duration-300 p-4 border rounded-xl font-normal text-18px hover:text-white hover:bg-blue focus:text-white focus:bg-blue' nameBtn='submitForm' valueBtn='submit' type='submit'>
+        <div className="flex flex-row text-center items-center my-10">
+          <Button
+            classNameBtn="w-9/12 bg-transparent border-blue border-dashed text-blue duration-300 p-4 border rounded-xl font-normal text-18px hover:text-white hover:bg-blue focus:text-white focus:bg-blue"
+            nameBtn="submitForm"
+            valueBtn="submit"
+            type="submit"
+          >
             Промокод для знижки
           </Button>
-          <p className='w-full items-center font-semibold text-xl'>Всього: {totalAmount} грн</p>
-          <Button classNameBtn='w-9/12 bg-gray-dark p-4 border rounded-xl font-bold text-18px text-white duration-300 hover:bg-transparent hover:text-black focus:bg-transparent focus:text-black' nameBtn='submitForm' valueBtn='submit' type='submit'>
+          <p className="w-full items-center font-semibold text-xl">Всього: {total} грн</p>
+          <Button
+            classNameBtn="w-9/12 bg-gray-dark p-4 border rounded-xl font-bold text-18px text-white duration-300 hover:bg-transparent hover:text-black focus:bg-transparent focus:text-black"
+            nameBtn="submitForm"
+            valueBtn="submit"
+            type="submit"
+          >
             Оплатити
           </Button>
         </div>
 
-        <hr className='mb-10 text-gray' />
+        <hr className="mb-10 text-gray" />
       </div>
     </m.div>
   );
